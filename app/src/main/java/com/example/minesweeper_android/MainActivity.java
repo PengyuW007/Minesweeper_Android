0ab57f9449058fd19ecda504938a1b2ca65b5b3a
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-05-12 13:18:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/minesweeper_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.minesweeper_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /********* Default setting***********/
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
